262d49c83ab05abefcf8913bac25aab15771c57f
Jan Wójcik <janwojcik5@gmail.com>
2016-04-10 23:54:42 +0200

    printing true/false table ready

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -19,6 +19,36 @@ class CSVArticleDao():
                 article.text = parts[4]
                 return article
 
+def print_true_false_table(articles_by_id,found_tags,number_of_tags):
+    false_negative=0
+    false_positive=0
+    true_negative=0
+    true_positive=0
+    for article in articles_by_id.values():
+	original_tag_set=set(article.tags)
+	found_tag_set=found_tags[article.id].copy()
+	
+	#clearing sets from empty tags (sometimes happens in file with tagged feeds)
+	if '' in original_tag_set:
+	  original_tag_set.remove('')
+	if '' in found_tag_set:
+	  found_tag_set.remove('')
+	#print original_tag_set,found_tag_set
+	
+	article_true_positive=0
+	for tag in original_tag_set:
+	    if tag in found_tag_set:
+	      article_true_positive+=1
+	      found_tag_set.remove(tag)
+	    else:
+	      false_negative+=1
+	for tag in found_tag_set:
+	  false_positive+=1
+	true_positive+=article_true_positive
+	true_negative+=(number_of_tags-article_true_positive)
+	    
+    print "\t\t\t found tags\tnot found tags\nactually present tags\t\t",true_positive,"\t",false_negative,"\nactually absent tags\t\t",false_positive,"\t",true_negative
+
 
 if __name__ == "__main__":
     tags_origin = TagsOrigin("./geomedia/cist-sample_geomedia-db/Sample_GeomediaDB/Dico_Country_Free.csv")
@@ -39,6 +69,15 @@ if __name__ == "__main__":
                 matching_tags_count += 1
                 break
     print(matching_tags_count / articles_count)
+    
+    #compute the number of tags
+    tag_set=set()
+    for tag in tags_origin.tags:
+      tag_set.add(tag.tag)
+    if '' in tag_set:
+      tag_set.remove('')
+    print_true_false_table(articles.create_articles_map(articles.articles),found_tags,len(tag_set))
+
 
     # if len(sys.argv) < 2:
     # for article in dao.get_articles('en_AUS_austra_int'):
