0ae5d950321168d20feff302c3de831ee0047b38
Jan Wójcik <janwojcik5@gmail.com>
2016-04-25 02:35:54 +0200

    Flawed chart showing.

--- Statistics.py
+++ Statistics.py
@@ -1,6 +1,11 @@
+# This Python file uses the following encoding: utf-8
+
 import collections
 import operator
 
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 
 class Statistics:
     def __init__(self, list_of_items):
@@ -23,3 +28,24 @@ class Statistics:
             print(tuple)
 
         print("total items count = " + str(sum(map(lambda x: x[1], sorted_by_occurrences))))
+        
+    def show_chart(self, printThreshold=0):
+        sorted_by_occurrences = sorted(self.stats.values())
+        sorted_by_occurrences.reverse()
+        sorted_by_occurrences = filter(lambda x: x > printThreshold, sorted_by_occurrences) 
+        
+        print sorted_by_occurrences
+        #sorted_by_occurrences=[1,1,2,2,10,15]
+        np_format_data=np.asarray(sorted_by_occurrences)
+        n, bins, patches = plt.hist(np_format_data,100)
+        print n
+        print bins
+	print patches
+	plt.plot(bins,n)
+	#plt.axis([0,200, 0, 500])
+	plt.ylim(0,75)
+	plt.title(u"Histogram częstości występowania tagów")
+	#plt.axis('scaled')
+	plt.xlabel('w ilu postach jednoczesnie jest tag')
+	plt.ylabel('ilosc tagow')
+	plt.show()
